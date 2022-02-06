0e3c9a31497a33156c147fa26d0c05bc1334b8e7
Israel Pérez <deki.wii@gmail.com>
2022-02-06 12:57:15 +0100

    creado y comprobado el test de getDepositoNivel

--- DepositoCombustible/src/MainTest.java
+++ DepositoCombustible/src/MainTest.java
@@ -3,10 +3,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDepositoNivel() {
+		DepositoCombustible dep = new DepositoCombustible(20.0,5.0);
+		double res = dep.getDepositoNivel();
+		assertEquals(5.0, res);
 	}
 
 }
